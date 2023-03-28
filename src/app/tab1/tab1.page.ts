72219a3045aaf87756c22da4d1d1b2e04142961d
ArtN0nymous <jr616934@gmail.com>
2023-03-28 13:02:24 -0700

    Revert "pipe for url img vista cartelera"
    This reverts commit 92fdeb9468fa0643953dea62437b07de44cdeb54.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Pelicula, Respuesta } from '../interfaces/interfaces';
 import { DataService } from '../services/data.service';
 
 @Component({
@@ -8,15 +7,9 @@ import { DataService } from '../services/data.service';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page implements OnInit {
-  peliculasCartelera:Pelicula[]=[];
-  slideOpts={
-    slidesPerView:1.5,
-    freeMode:true
-  }
+
   constructor(private dataService:DataService) {}
   ngOnInit(): void {
-      this.dataService.getCartelera().subscribe(res=>{
-        this.peliculasCartelera=res.results;
-      });
+      this.dataService.getCartelera().subscribe(console.log);
   }
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiKey:"d1d64f1f44c48a9613f37fa1d87b259f",
+  apiUrl:"https://api.themoviedb.org/3"
+};
