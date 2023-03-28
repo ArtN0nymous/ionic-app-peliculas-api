881cf213b910be32d8ba2d31f7f2d29f90719f8a
Ramsus <77692202+ArtN0nymous@users.noreply.github.com>
2023-03-28 13:58:42 -0600

    enviroments

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiKey:"d1d64f1f44c48a9613f37fa1d87b259f",
-  apiUrl:"https://api.themoviedb.org/3",
-  defaultiMG:"https://coffective.com/wp-content/uploads/2018/06/default-featured-image.png.jpg"
-};
